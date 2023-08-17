58534172875d8c85497afb2044287c598edeb49f
dpurcell <dpurcell.pd@gmail.com>
2023-08-17 11:02:08 -0400

    tip calculator express application created

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`<h1>Hello ${req.params.name}! It's so great to see you!</h1>`)
 });
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    const tip = req.params.total *  (req.params.tipPercentage / 100)
+    res.send(`<h1>Your tip today is $${tip}.</h1>`);
+})
+
 
 
 app.listen(port, ()=> {
