1f2ca8bbca7defc2b0d9cd8dd1cabfdba6261112
dpurcell <dpurcell.pd@gmail.com>
2023-08-17 11:38:23 -0400

    added default greeting

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/greeting/:name', (req, res) => {
-    res.send(`<h1>Hello ${req.params.name}! It's so great to see you!</h1>`)
-});
-
-app.get('/tip/:total/:tipPercentage', (req, res) => {
-    const tip = req.params.total *  (req.params.tipPercentage / 100)
-    res.send(`<h1>Your tip today is $${tip}.</h1>`);
+app.get("/greeting", (req, res) => {
+    res.send("<h1>Hello, stranger!/h1>")
 })
 
+app.get("/greeting/:name", (req, res) => {
+  res.send(`<h1>Hello ${req.params.name}! It's so great to see you!</h1>`);
+});
 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+  const tip = req.params.total * (req.params.tipPercentage / 100);
+  res.send(`<h1>Your tip today is $${tip}.</h1>`);
+});
 
-app.listen(port, ()=> {
-    console.log(`Listening on port ${port}`);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
